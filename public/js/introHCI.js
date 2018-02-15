a4f2b8eff56d206ae07d77887c038b37447a95bb
Zehua Chen <zec023@ucsd.edu>
2018-02-15 06:03:27 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
 	$('#colorBtn').click(randomizeColors);
 }
 
@@ -25,6 +24,18 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get("http://localhost:3000/project/"+idNumber, addDetails)
+	//console.log("User clicked on project " + idNumber);
+}
 
-	console.log("User clicked on project " + idNumber);
+function addDetails(result) {
+	//console.log(result);
+	var projectHTML = '<img src=' + result.image + ' class=detailsImage>' +
+		'<h4>' + result.title + '</h4>' + '<h5>' + result.date + '</h5>' +
+		result.summary; 
+	$("#project"+result.id.toString()).find(".details").html(projectHTML);
+}
+
+function randomizeColors() {
+	
 }
